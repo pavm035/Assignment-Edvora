710b66cc35188c44a1c30ad9cf34a773fc102388
Pavan <kumar.pavan@rakuten.com>
2022-02-10 19:38:42 +0900

    Initial Commit

---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by Mahadevaiah, Pavan | Pavan | ECMPD on 2022/02/10.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
